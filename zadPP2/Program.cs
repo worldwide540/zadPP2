8ec8aeacbba8389577ad866ff3fb6034c91bf2a8
Нарек Антонян <narekantonyan2033@gmail.com>
2024-06-20 17:37:03 +0300

    Добавьте файлы проекта.

---
+++ zadPP2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class TimeConverter
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите время в формате ЧЧ:ММ AM/PM:");
+        string inputTime = Console.ReadLine();
+
+        DateTime time;
+        if (DateTime.TryParse(inputTime, out time))
+        {
+            Console.WriteLine("Время в 24-часовом формате: " + time.ToString("HH:mm"));
+            Console.WriteLine("Время в 12-часовом формате: " + time.ToString("hh:mm tt"));
+        }
+        else
+        {
+            Console.WriteLine("Некорректный формат времени.");
+        }
+    }
+}
